f26675e7c62a7d5246f1d77c23c2c2a781e6c710
AnchaMoe <169299971@qq.com>
2020-09-16 18:27:36 +0800

    Change Color

--- SystemTray.sh
+++ SystemTray.sh
@@ -3,5 +3,8 @@
 
 killall trayer
 if [ $? != 0 ]; then
-	trayer --edge top --align right --widthtype pixel --width 330 --heighttype pixel --height 30 --SetPartialStrut true --transparent true --alpha 180
+	trayer --edge top --align right \
+	--widthtype pixel --width 330 --heighttype pixel --height 30 \
+	--SetPartialStrut true --transparent true \
+	--alpha 180 --tint 0x66666666
 fi
