5c70a84875f3e8e395eff1c6b487c24e8d592995
AnchaMoe <169299971@qq.com>
2020-09-16 18:19:23 +0800

    First Commit

---
+++ SystemTray.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# A system tray by "trayer".
+
+killall trayer
+if [ $? != 0 ]; then
+	trayer --edge top --align right --widthtype pixel --width 330 --heighttype pixel --height 30 --SetPartialStrut true --transparent true --alpha 180
+fi
